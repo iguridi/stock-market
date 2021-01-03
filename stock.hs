905adef3866f62085774671e1475a41c0301c00e
iguridi <iguridi@uc.cl>
2021-01-03 13:10:01 -0300

    first functions of haskell implementation

---
+++ stock.hs
@@ -0,0 +1,33 @@
+offerMargin :: Int
+offerMargin = 4
+
+
+deleteFirst _ [] = []
+deleteFirst a (b:bc) | a == b    = bc
+                     | otherwise = b : deleteFirst a bc
+
+maxNumber :: Int -> Int -> Int
+maxNumber a b =
+    if a >= b then a
+    else b
+
+addToList :: Int -> [Int] -> [Int]
+addToList a [] = [a]
+addToList a (x:xs) = x : addToList a xs
+
+
+addBuyOffer :: [Int] -> Int -> [Int]
+addBuyOffer buying price = addToList (maxNumber (price - offerMargin) 1) buying
+
+main = do
+    let res = offerMargin
+    putStrLn $ "1+2+3 = " ++ show res
+
+    let res = deleteFirst 4 [1..10]
+    putStrLn $ "no 4 = " ++ show res
+
+    let res = maxNumber 6 5
+    putStrLn $ "max number = " ++ show res
+
+    let res = addBuyOffer [1..5] 6
+    putStrLn $ "max number = " ++ show res
