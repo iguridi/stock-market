fe9f13c63a8b130719b671d69f0988580f2767b6
Ignacio Guridi <iguridi@fb.com>
2022-12-29 15:23:17 -0300

    working chart yey

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,12 +1,12 @@
 import Control.Monad (forM_, mapM_)
 import System.Random ( mkStdGen, Random(randomRs) )
-import Data.List
+import Data.List ( transpose )
 
 
 
 createColumn :: Int -> Int -> [Char]
-createColumn position length =
-  let spaces = replicate length ' '
+createColumn position height =
+  let spaces = replicate height ' '
       line = take (position - 1) spaces ++ "*" ++ drop position spaces
    in line
 
@@ -15,25 +15,25 @@ createXaxis length =
   replicate length '-'
 
 genColumns :: Int -> [Int] -> [[Char]]
-genColumns length values = do
-  let joinColumns x acc = x ++ [createColumn acc length]
+genColumns height values = do
+  let joinColumns x acc = x ++ [createColumn acc height]
   foldl joinColumns [] values
 
 
 chart :: IO ()
 chart = do
-  -- let position = 10
   let length = 20
-  -- let array = [1, 2, 3, 4, 5]
+  let height = 5
   let generator = mkStdGen 42
-  let randomNumbers = take 10 (randomRs (1, 5) generator)
-  let columns = genColumns length randomNumbers
+  let randomNumbers = take 10 (randomRs (1, height) generator)
+  let columns = genColumns height randomNumbers
   let transposed = transpose columns
+  let withYAxis = map ('|':) transposed
 
   let xAxis = " " ++ createXaxis length
+  let chart = withYAxis ++ [xAxis]
   putStrLn "\n\n"
-  mapM_ print transposed
-  putStrLn xAxis
+  putStrLn (unlines chart)
 
 main :: IO ()
 main = do
