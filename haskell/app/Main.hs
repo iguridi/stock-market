1d752049bdec0c842d37ba53f5a5bf6758ada8e8
Ignacio Guridi <iguridi@fb.com>
2022-12-29 15:54:35 -0300

    remove unused

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,15 +1,10 @@
-import Control.Monad (forM_, mapM_)
 import Data.List (transpose)
 import System.Random (Random (randomRs), mkStdGen)
 
 createColumn :: Int -> Int -> [Char]
-createColumn columnPosition chartHeight =
-  reverse [if y == columnPosition then '*' else ' ' | y <- [1 .. chartHeight]]
+createColumn value chartHeight =
+  reverse [if y == value then '*' else ' ' | y <- [1 .. chartHeight]]
 
-
--- createXaxis :: Int -> String
--- createXaxis chartWidth =
---   replicate chartWidth '-'
 
 genColumns :: Int -> [Int] -> [[Char]]
 genColumns chartHeight values = do
@@ -20,9 +15,10 @@ chart :: IO ()
 chart = do
   let chartWidth = 10
   let chartHeight = 5
+
   let generator = mkStdGen 42
-  -- let randomNumbers = take 10 (randomRs (1, chartHeight) generator)
   let randomNumbers = [1, 2, 3, 4, 4, 3, 2, 1, 5]
+
   let columns = genColumns chartHeight randomNumbers
   let transposed = transpose columns
   let withYAxis = map ('|' :) transposed
