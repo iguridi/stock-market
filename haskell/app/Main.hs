ebaeee2a9855cc41cc4ffcf088f61fdd4fc11e3b
Ignacio Guridi <iguridi@fb.com>
2022-12-29 15:30:29 -0300

    fix orientation

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,36 +1,33 @@
 import Control.Monad (forM_, mapM_)
-import System.Random ( mkStdGen, Random(randomRs) )
-import Data.List ( transpose )
-
-
+import Data.List (transpose)
+import System.Random (Random (randomRs), mkStdGen)
 
 createColumn :: Int -> Int -> [Char]
-createColumn position height =
-  let spaces = replicate height ' '
-      line = take (position - 1) spaces ++ "*" ++ drop position spaces
-   in line
+createColumn columnPosition chartHeight =
+  reverse [if y == columnPosition then '*' else ' ' | y <- [1 .. chartHeight]]
+
 
 createXaxis :: Int -> String
-createXaxis length =
-  replicate length '-'
+createXaxis chartWidth =
+  replicate chartWidth '-'
 
 genColumns :: Int -> [Int] -> [[Char]]
-genColumns height values = do
-  let joinColumns x acc = x ++ [createColumn acc height]
+genColumns chartHeight values = do
+  let joinColumns x acc = x ++ [createColumn acc chartHeight]
   foldl joinColumns [] values
-
 
 chart :: IO ()
 chart = do
-  let length = 20
-  let height = 5
+  let chartWidth = 10
+  let chartHeight = 5
   let generator = mkStdGen 42
-  let randomNumbers = take 10 (randomRs (1, height) generator)
-  let columns = genColumns height randomNumbers
+  -- let randomNumbers = take 10 (randomRs (1, chartHeight) generator)
+  let randomNumbers = [1, 2, 3, 4, 4, 3, 2, 1, 5]
+  let columns = genColumns chartHeight randomNumbers
   let transposed = transpose columns
-  let withYAxis = map ('|':) transposed
+  let withYAxis = map ('|' :) transposed
 
-  let xAxis = " " ++ createXaxis length
+  let xAxis = " " ++ createXaxis chartWidth
   let chart = withYAxis ++ [xAxis]
   putStrLn "\n\n"
   putStrLn (unlines chart)
