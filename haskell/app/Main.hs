f353843653d09014946ed41a3ebc5d4e6e882018
Ignacio Guridi <iguridi@fb.com>
2022-12-29 18:45:38 -0300

    haskell chart with ranges o boi

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -3,35 +3,49 @@ import System.Random (Random (randomRs), mkStdGen)
 
 numberOfOffers :: Int
 numberOfOffers = 10
--- TIME = 75
 
-createColumn :: Int -> Int -> [Char]
-createColumn value chartHeight =
-  reverse [if y == value then '*' else ' ' | y <- [1 .. chartHeight]]
+time :: Int
+time = 75
 
+toChar :: Int -> (Int, Int) -> Char
+toChar x range = case x of
+  _
+    | x > fst range && x < snd range -> '|'
+    | x == fst range || x == snd range -> '+'
+    | otherwise -> ' '
 
-genColumns :: Int -> [Int] -> [[Char]]
+createColumn :: Int -> (Int, Int) -> [Char]
+createColumn chartHeight range =
+  reverse [toChar y range | y <- [1 .. chartHeight]]
+
+genColumns :: Int -> [(Int, Int)] -> [[Char]]
 genColumns chartHeight values = do
-  let joinColumns x acc = x ++ [createColumn acc chartHeight]
+  let joinColumns x acc = x ++ [createColumn chartHeight acc]
   foldl joinColumns [] values
-
 
 simulation = do
   let generator = mkStdGen 42
-  let biding = take NUMBER_OF_ORDERS (randomRs (4, 25) generator)
-  let asking = take NUMBER_OF_ORDERS (randomRs (25, 50) generator)
+  -- let biding = take NUMBER_OF_ORDERS (randomRs (4, 25) generator)
+  -- let asking = take NUMBER_OF_ORDERS (randomRs (25, 50) generator)
+  let history = replicate time (1, 50)
+  -- let lastPrice = minimum asking
+  -- let delta = 0
+
+  -- turns
+  -- graph
+  chart history
 
 
-chart :: IO ()
-chart = do
-  let chartWidth = 10
-  let chartHeight = 5
+chart :: [(Int, Int)] -> IO ()
+chart history = do
+  let chartHeight = maximum (map maximum history)
+  let chartWidth = time
 
-  let generator = mkStdGen 42
+  -- let generator = mkStdGen 42
   -- let randomNumbers = take 10 (randomRs (1, chartHeight) generator)
-  let randomNumbers = [1, 2, 3, 4, 4, 3, 2, 1, 5]
+  -- let randomNumbers = [1, 2, 3, 4, 4, 3, 2, 1, 5]
 
-  let columns = genColumns chartHeight randomNumbers
+  let columns = genColumns chartHeight history
   let transposed = transpose columns
   let withYAxis = map ('|' :) transposed
 
@@ -42,4 +56,4 @@ chart = do
 
 main :: IO ()
 main = do
-  chart
+  simulation
