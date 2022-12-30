90ba6a172d88e45f8e2f94e38a1a3a4459ef9b17
Ignacio Guridi <iguridi@fb.com>
2022-12-30 08:31:50 -0300

    we have a simulation loop now

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,11 +1,12 @@
+import Control.Monad (when)
 import Data.List (transpose)
 import System.Random (Random (randomRs), mkStdGen)
 
 numberOfOffers :: Int
 numberOfOffers = 10
 
-time :: Int
-time = 75
+totalTurns :: Int
+totalTurns = 75
 
 toChar :: Int -> (Int, Int) -> Char
 toChar x range = case x of
@@ -23,23 +24,30 @@ genColumns chartHeight values = do
   let joinColumns x acc = x ++ [createColumn chartHeight acc]
   foldl joinColumns [] values
 
-simulation = do
+oneTurn :: ([Int], [Int], [(Int, Int)], Int, Int) -> Int -> ([Int], [Int], [(Int, Int)], Int, Int)
+oneTurn (biding, asking, history, lastPrice, delta) time = (biding, asking, history, lastPrice, delta)
+
+makeOffers :: (Int, Int) -> [Int]
+makeOffers range = do
   let generator = mkStdGen 42
-  -- let biding = take NUMBER_OF_ORDERS (randomRs (4, 25) generator)
-  -- let asking = take NUMBER_OF_ORDERS (randomRs (25, 50) generator)
-  let history = replicate time (1, 50)
-  -- let lastPrice = minimum asking
-  -- let delta = 0
+  take numberOfOffers (randomRs range generator)
+
+simulation = do
+  let biding = makeOffers (1, 25) --take numberOfOffers (randomRs (1, 25) generator)
+  let asking = makeOffers (25, 50) --take numberOfOffers (randomRs (25, 50) generator)
+  let history = replicate totalTurns (1, 50)
+  let lastPrice = minimum asking
+  let delta = 0
 
   -- turns
+  let (biding', asking', history', lastPrice', delta') = foldl oneTurn (biding, asking, history, lastPrice, delta) [1 .. totalTurns]
   -- graph
-  chart history
-
+  chart history'
 
 chart :: [(Int, Int)] -> IO ()
 chart history = do
   let chartHeight = maximum (map maximum history)
-  let chartWidth = time
+  let chartWidth = totalTurns
 
   -- let generator = mkStdGen 42
   -- let randomNumbers = take 10 (randomRs (1, chartHeight) generator)
