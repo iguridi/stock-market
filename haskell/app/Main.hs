b8998703dc32d74f94f545daa245dbf866d52e38
Ignacio Guridi <iguridi@fb.com>
2023-01-05 12:52:48 -0300

    almost there boi

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -8,7 +8,7 @@ numberOfOffers :: Int
 numberOfOffers = 10
 
 totalTurns :: Int
-totalTurns = 5
+totalTurns = 75
 
 toChar :: Int -> (Int, Int) -> Char
 toChar x range = case x of
@@ -43,34 +43,45 @@ replaceNth n f (x:xs)
   | n == 0 = f x:xs
   | otherwise = x:replaceNth (n-1) f xs
 
--- updateHistory x y = trace ("DEBUG: updateHistory" ++ show x) (reverse ("bob" ++ x))
 
 updateHistory :: Int -> (Int, Int) -> (Int, Int)
-updateHistory price (minHist, maxHist) =
-  trace ("DEBUG: updateHistory  " ++ show price ++ "   " ++ show (minHist, maxHist)) (
-    min price minHist, max price maxHist
-  )
+updateHistory price (minHist, maxHist) = (min price minHist, max price maxHist)
+
 
 bid :: ([Int], [Int], [(Int, Int)], Int, Int) -> Int -> Int -> ([Int], [Int], [(Int, Int)], Int, Int)
-bid (biding, asking, history, lastPrice, delta) time margin = trace ("DEBUG: bid  \n" ++ show history ++ "\n" ++ show lastPrice ++ "\n" ++ show delta) (do
+bid (biding, asking, history, lastPrice, delta) time margin =
+  -- trace ("DEBUG: bid  \n" ++ show biding ++ "\n" ++ show asking ++ "\n" ++ show history ++ "\n" ++ show lastPrice ++ "\n" ++ show delta ++ "\n\n") (
+    do
   let price = minimum asking
   let asking' = replaceX asking price (margin + maximum biding)
   let history' = replaceNth time (updateHistory price) history
-  (biding, asking', history', price, price - lastPrice))
+  (biding, asking', history', price, price - lastPrice)
+  -- )
 
 ask :: ([Int], [Int], [(Int, Int)], Int, Int) -> Int -> Int -> ([Int], [Int], [(Int, Int)], Int, Int)
-ask (biding, asking, history, lastPrice, delta) time margin = do
+ask (biding, asking, history, lastPrice, delta) time margin =
+  -- trace ("DEBUG: ask  \n" ++ show biding ++ "\n" ++ show asking ++ "\n" ++ show history ++ "\n" ++ show lastPrice ++ "\n" ++ show delta ++ "\n" ++ show margin ++ "\n\n") (
+    do
   let price = minimum biding
   let biding' = replaceX biding price (margin + minimum asking)
   let history' = replaceNth time (updateHistory price) history
   (biding', asking, history', price, price - lastPrice)
+  -- )
 
 
 oneTurn :: ([Int], [Int], [(Int, Int)], Int, Int) -> (Int, Bool, Int) -> ([Int], [Int], [(Int, Int)], Int, Int)
-oneTurn info turn = do
-  let (time, random, margin) = turn
-  let decision = if random then ask else bid
-  decision info time margin
+oneTurn info turn =
+  -- trace ("DEBUG: oneTurn  \n" ++ show info ++ "\n" ++ show turn ++ "\n") (
+    do
+      let (_, _, _, _, delta) = info
+      let (time, random, margin) = turn
+      let randomDecision = if random then ask else bid
+      let info' = randomDecision info time margin
+      let tendency = if delta > 0 then ask else bid
+      let info'' = tendency info' time margin
+      let info''' = tendency info'' time margin
+      info'''
+  -- )
 
 randomSequence :: Int -> Int -> Int -> IO [Int]
 randomSequence total from to = replicateM total  $ randomRIO (from, to :: Int)
@@ -81,11 +92,12 @@ simulation = do
   let asking = take numberOfOffers (randomRs (25, 50) (mkStdGen 41))
   -- biding <- randomSequence numberOfOffers 1 25
   -- asking <- randomSequence numberOfOffers 25 50
-  let info = (biding, asking, replicate totalTurns (1, 50), minimum asking, 0)
+  let info = (biding, asking, replicate totalTurns (50, 1), minimum asking, 0)
 
   -- turns
   let randomnActions = take totalTurns $ randoms (mkStdGen 11) :: [Bool]
-  let randomnMargins = take totalTurns $ randoms (mkStdGen 12) :: [Int]
+  let randomnMargins = take numberOfOffers (randomRs (0, 10) (mkStdGen 41))
+  -- randomMargins <- randomSequence numberOfOffers 25 50
   let turns = zip3 [1 .. totalTurns] randomnActions randomnMargins
   let (biding', asking', history', lastPrice', delta') = foldl oneTurn info turns
   -- graph
